d53c103e3c01540a2663e69e3cd4acc65b1bc13a
Steven Penny <svnpenn@gmail.com>
2014-10-01 21:08:28 -0500

    x86_64-w64-mingw32-windres suede.rc suede.o

--- release.sh
+++ release.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 vr=$(git log --oneline | wc -l)
-x86_64-w64-mingw32-windres -O coff suede.rc suede.res
-x86_64-w64-mingw32-gcc -Wall -Wextra -pedantic -std=c99 \
-  -mwindows -osuede suede.c suede.res
+x86_64-w64-mingw32-windres suede.rc suede.o
+x86_64-w64-mingw32-gcc -s -Wall -Wextra -pedantic -std=c99 \
+  -mwindows -osuede suede.c suede.o
 zip -9r suede-$vr.zip suede.exe README.md
